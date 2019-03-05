9e843237d542f0f67ac1bd2dcb2282a02142fe92
Michael Wells <mwells@sygnal.com>
2019-03-06 11:47:11 +1300

    Clean hot

--- dist/dist/src/services/quotes/quotes.service.d.ts
+++ dist/dist/src/services/quotes/quotes.service.d.ts
@@ -2,7 +2,4 @@ import { HttpService } from '@nestjs/common';
 export declare class QuotesService {
     private http;
     constructor(http: HttpService);
-    getQuotes(): import("rxjs").Observable<any>;
-    getQuote(id: any): import("rxjs").Observable<any>;
-    getRandomQuote(): import("rxjs").Observable<any>;
 }
